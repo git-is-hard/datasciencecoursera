22494a5853e0cdcf23242b82ea7790d95d0c6951
git-is-hard <thaitran1410@icloud.com>
2022-03-18 12:16:23 +0700

    coursera data science week 2, 3rd

--- specdata/complete.R
+++ specdata/complete.R
@@ -1,5 +1,7 @@
 complete <- function(directory, id = 1: 332){
   y = c()
+  x = c()
+  nobsList = c()
 
   setwd("~/r_project/coursera_course/datasciencecoursera")
   setwd(directory)
@@ -9,11 +11,23 @@ complete <- function(directory, id = 1: 332){
   
   #load csv file that need to open
   loadedCSV <- lapply(fileName[id], read.csv)
-
-  for (i in c(1:(sum(!is.na(id))))){smallY = sum(!is.na(loadedCSV[i][[1]]$sulfate))
+  
+  for (i in c(1:length(id))){smallY = sum(!is.na(loadedCSV[i][[1]]$sulfate))
     y[i] <- smallY
+  }
+  for (i in c(1:length(id))){smallX = sum(!is.na(loadedCSV[i][[1]]$nitrate))
+    x[i] <- smallX
+  }
+  for (i in c(1:length(id))){
+    if (x[i] < y[i]){
+      nobsList[i] <- x[i]
     }
-  df <- data.frame("id" = c(id), "nobs" = y  )
+    if (x[i] > y[i]){
+      nobsList[i] <- y[i]
+    }
+    if (x[i] == y[i]){
+      nobsList[i] <- x[i]
+    }
+  }
+  df <- data.frame("id" = c(1: length(id)), "nobs" = nobsList  )
 df}
-
-
--- specdata/corr.R
+++ specdata/corr.R
@@ -1,1 +1,15 @@
 corr <- function(directory, threshold = 0) {
+  setwd("~/r_project/coursera_course/datasciencecoursera")
+  loadComplete <- complete(directory)
+  checkID <- loadComplete[loadComplete$nobs>threshold, ]
+  corDATA = c()
+  for (case in checkID$id){
+      loadedCSV <- read.csv(paste(sprintf("%03d",case),".csv", sep = ""))
+      cleanNA_sulfate <- loadedCSV[(!is.na(loadedCSV$sulfate)),]
+      cleanDATA <- cleanNA_sulfate[(!is.na(cleanNA_sulfate$nitrate)),]
+      dataSulfate = cleanDATA["sulfate"]
+      dataNitrate = cleanDATA["nitrate"]
+      corDATA = c(corDATA, cor(dataSulfate, dataNitrate))
+  }
+corDATA
+}
